e6c2a67ae2640b1f0eff6062478f2e3bc3eab96f
m000878 <Mitomacie55>
2022-03-15 17:20:56 +0200

    Project structure creation (wrapper added to avoid mvn installation)

---
+++ src/test/java/com/bdrecruit/springbootmsbdrc/SpringbootMsBdrcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdrecruit.springbootmsbdrc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMsBdrcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
